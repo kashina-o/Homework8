364fbfc2cdc149b14d0ababaf51999814d7331ce
Olga <poteshkina-o99@mail.ru>
2023-03-08 17:52:30 +0500

    add Task 60

---
+++ Task60/Task60.cs
@@ -0,0 +1,57 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int Input(string msg)
+{
+  bool flag = false;
+  int value = 0;
+  while (!flag)
+  {
+    Console.Write(msg + " ");
+    flag = int.TryParse(Console.ReadLine(), out value);
+  }
+  return value;
+}
+
+int[,,] CreateArray(int x, int y, int z)
+{
+    int[,,] matrix = new int[x, y, z];
+    return matrix;
+}
+
+void FillMatrix(int[,,] matrix)
+{
+    int count = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+void PrintIndex(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+int x = Input("Введите X: ");
+int y = Input("Введите Y: ");
+int z = Input("Введите Z: ");
+int[,,] matrix = CreateArray(x, y, z);
+FillMatrix(matrix);
+PrintIndex(matrix);
